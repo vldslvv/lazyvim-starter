aaf218e55881ba59d3299a0d05f6e5edb7c5ca3c
Vladislav Vyatkin <vyatkinvladislav@gmail.com>
2024-11-27 01:53:05 +0200

    Disable flash keymap

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,7 +1,10 @@
+M = require("helpers")
+
 return {
   {
     "folke/flash.nvim",
     keys = {
+      M.disable_keymap("S", "Disable standard flash key"),
       { "<a-s>", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     },
   },
