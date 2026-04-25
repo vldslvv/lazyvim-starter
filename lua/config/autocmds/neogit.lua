de2f6dfb5669df114a3c2d6ba69b0820f79fe6d1
Vladislav Vyatkin <vyatkinvladislav@gmail.com>
2026-04-25 17:46:56 -0300

    Search untracked files for neogit

--- lua/config/autocmds/neogit.lua
+++ lua/config/autocmds/neogit.lua
@@ -4,15 +4,15 @@ local M = {}
 function SetNeogitKeymaps()
   vim.api.nvim_buf_set_keymap(0, "n", "<leader>ju", ":SearchUnstagedChanges<CR>", { noremap = true, silent = true })
   vim.api.nvim_buf_set_keymap(0, "n", "<leader>js", ":SearchStagedChanges<CR>", { noremap = true, silent = true })
+  vim.api.nvim_buf_set_keymap(0, "n", "<leader>jt", ":SearchUntrackedFiles<CR>", { noremap = true, silent = true })
 end
 
 function M.setup()
   -- Neogit keymaps
   -- Define a command to search for 'unstaged changes'
-  -- vim.api.nvim_command('command! SearchUnstagedChanges execute "/unstaged changes"')
   vim.api.nvim_command('command! SearchUnstagedChanges execute ":g/^Unstaged changes" | normal! <Esc>')
-  -- vim.api.nvim_command('command! SearchStagedChanges execute "/unstaged changes"')
   vim.api.nvim_command('command! SearchStagedChanges execute ":g/^Staged changes" | normal! <Esc>')
+  vim.api.nvim_command('command! SearchUntrackedFiles execute ":g/^Untracked files" | normal! <Esc>')
 
   -- Create an autocmd group to avoid duplicate autocmds
   vim.api.nvim_command("augroup NeogitKeymaps")
